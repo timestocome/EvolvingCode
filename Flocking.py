8c2b1de765ac737dbf3afd90e73dffcd3e776895
Linda MacPhee-Cobb <timestocome@gmail.com>
2018-01-07 12:59:26 -0600

    meh, improvements to Flocking, but I'm not not happy with it

--- Flocking.py
+++ Flocking.py
@@ -1,11 +1,14 @@
 
 # http://github.com/timestocome
 
+# Flocking take 2
+
 # Flocking, herds and schools
 # https://www.siggraph.org/education/materials/HyperGraph/animation/art_life/flocks.htm
 
 # more good stuff
 # https://www.siggraph.org/education/materials/HyperGraph/animation/art_life/art_life0.htm
+# http://www.red3d.com/cwr/papers/1987/SIGGRAPH87.pdf
 
 # Flock is a group of objects that exhibit the general class of polarized (aligned), non-colliding, aggregate motion.
 # Boid is a simulated bird-like object, i.e., it exhibits this type of behavior. It can be a fish, dinosaur, etc.
@@ -16,149 +19,94 @@
 # Flock centering: attempt to stay close to neighbors
 
 
-# experimented a little
-# hard edges produces flocking
-# wrap around edges produces more of schooling behavior
-# 
-# sometimes they move fast
-# sometimes they travel the edges
-# other times they all settle in clumps
-
 
 import numpy as np
-from scipy import signal
 import matplotlib.pyplot as plt
 from matplotlib import animation
 import copy
-
 
 
 # create a 2d board to use for simulation
 height = 300
 width = height
 
-
-# animation setup
 # how many time steps animation should perform
 n_steps = 10000
 sleep = 1
 
 
 # set number of boids
-n_boids = 100
+n_boids = 140
 
+# distance neighbors are visable
+radius = 30
 
-# limits
-min_velocity = 1
-max_velocity = 10
-
-min_distance = 2
-
-
-# constants
-north = 0           # y-1
-north_east = 1      # y-1, x+1
-east = 2            # x+1
-south_east = 3      # x+1, y+1
-south = 4           # y+1
-south_west = 5      # y+1, x-1
-west = 6            # x-1
-north_west = 7      # x-1, y-1 
+# max number of pixels moved per time step
+max_speed = 12
 
 
 
+
+
+# smoother
+def deg_to_rad(d): return d / 57.3
+def rad_to_deg(r): return r * 57.3
+
+n_directions = 360
+directions = range(0, 360)
 
 ############################################################################
 # utilities
 ############################################################################
 
-def new_location(x, y, v, d):
+# not optimal but it's a toy problem
+def neighborhood(x, y, boids):
+
+    neighbors = []
+     
+    for b in boids:
+
+        d = b.d % 360
+
+        # see if other boid is close to us
+        dx = np.sqrt( (b.x-x)*(b.x-x) + (b.y-y)*(b.y-y) )
+        
+        # it's inside circle
+        if dx <= radius:
+
+            # see if it's in ~180' of direction we're facing
+            if (d > 45) and (d < 135):    # N  
+                if b.y > y: neighbors.append(b)
+                
+            if (d > 0) and (d < 90):    # NE   equation written out for clarity
+                if b.y > (-1)*b.x + (x+y): neighbors.append(b) 
+                
+            if ((d > 315) and (d < 360)) or ((d > 0) and (d < 45)):    # E 
+                if b.x > x: neighbors.append(b)
+                
+            if (d > 270) and (d < 360):    # SE 
+                if b.y < b.x + (y-x): neighbors.append(b)
+                
+            if (d > 225) and (d < 315):    # S 
+                if b.y < y: neighbors.append(b)
+                    
+            if (d > 180) and (d < 270):    # SW 
+                if b.y < -b.x + (x+y): neighbors.append(b)
+                
+            if (d > 135) and (d < 225):    # W 
+                if b.x < x: neighbors.append(b)
+                
+            if (d < 90) and (d < 180):    # NW
+                if b.y > b.x + (y-x): neighbors.append(b)
 
 
-    if v < min_velocity: v += 1
-    if v > max_velocity: v -= 1
+    return neighbors
 
 
-    edge = width - (3 * v)
+
+
+
     
-
-    if d == north:
-        if (y - v) > 0: y -= v 
-        else: 
-            y = v
-            d = np.random.randint(0, 7)
-
-    elif d == north_east:
-        if (y - v) > 0: y -= v 
-        else: 
-            y = v
-            d = np.random.randint(0, 7)
-
-
-        if (x + v) < edge: x += v
-        else: 
-            x = edge
-            d = np.random.randint(0, 7)
-
-    elif d == east:
-        if (x + v) < edge: x += v
-        else: 
-            x = edge
-            d = np.random.randint(0, 7)
-
-    elif d == south_east:
-        if (y + v) < edge: y += v
-        else: 
-            y = edge
-            d = np.random.randint(0, 7)
-
-        if (x + v) < edge: x += v
-        else: 
-            x = edge
-            d = np.random.randint(0, 7)
-
-    elif d == south:
-        if (y + v) < edge: y += v
-        else: 
-            y = edge
-            d = np.random.randint(0, 7)
-
-    elif d == south_west:
-        if (y + v) < edge: y += v
-        else: 
-            y = edge
-            d = np.random.randint(0, 7)
-
-        if (x - v) > 0: x -= v
-        else: 
-            x = v
-            d = np.random.randint(0, 7)
-
-
-    elif d == west:
-        if (x - v) > 0: x -= v
-        else: 
-            x = v
-            d = np.random.randint(0, 7)
-
-
-    elif d == north_west:
-        if (y - v) > 0: y -= v
-        else: 
-            y = v
-            d = np.random.randint(0, 7)
-
-
-        if (x - v) > 0: x -= v
-        else: 
-            x = v
-            d = np.random.randint(0, 7)
-
-
-    return int(x), int(y), v, d
-
-
-
 
 ############################################################################
 # Flocking
@@ -169,12 +117,108 @@ class Boid(object):
 
         self.x = np.random.randint(0, width-1)
         self.y = np.random.randint(0, height-1)
-        self.velocity = np.random.randint(min_velocity, max_velocity)
-        self.direction = np.random.randint(0, 7)
+        self.d = np.random.randint(n_directions)
+        self.v = np.random.randint(max_speed)
+        
+
+    def update(self, neighbors):
+
+        # adjust to neighbors
+        n_neighbors = len(neighbors)
+
+        if n_neighbors > 0:
+
+            dv = 0.
+            dd = np.zeros(n_directions)
+            dx = 0.
+            dy = 0.
+            crowding = 0
+            
+            for n in neighbors:
+ 
+                # alignment: get average heading and speed
+                dv += n.v / n_neighbors
+                dd[int(n.d)] += 1
+               
+                        
+                # separation: check for crowding and spread out
+                dr = np.sqrt( (n.x-self.x)*(n.x-self.x) + (n.y-self.y)*(n.y-self.y) )
+                if dr < 5:
+                    crowding += 1
+
+                else:
+                    
+                    # cohesion: center of flock
+                    dx += n.x / n_neighbors
+                    dy += n.y / n_neighbors
+
+
+                
+            # alignment: change heading closer to neighbors
+            popular = np.max(dd)               # find n max agreeing
+            winners = np.argwhere(dd == popular)   # get most popular headings
+
+            if len(winners) > 1:               # if more than one randomly choose one
+                new_idx = np.random.randint(len(winners))
+                self.d = winners[new_idx]
+            else: self.d = np.argmax(dd)      # else just go with single most popular
+
+                        
+            
+            # cohesion: move towards center of flock
+            if self.x > dx: dx -= self.v // 2
+            else: dx += self.v 
+
+            if self.y > dy: dy -= self.v // 2
+            else: dy += self.v
+            
+
+           
+            
+            #print(' d %d, v %d neighbors %d' %( self.d, self.v, n_neighbors))
+            
+        else:   # no neighbors occasionally decide to change direction or move
+            z = np.random.randint(max_speed * 2)
+            if z % 4 == 0:
+                self.v = z
+                self.d = np.random.randint(n_directions)
+
+
+        
+
+        # move in direction boid is facing
+        dx = int(self.v * np.cos(deg_to_rad(self.d)))
+        self.x += dx
+                  
+        dy = int(self.v * np.sin(deg_to_rad(self.d)))
+        self.y += dy
 
 
 
+        # wrap on or rebound at edges
+        new_d = np.random.randint(n_directions)
 
+        if self.x <= 0:
+            self.x = self.v
+            self.d = new_d
+            
+        if self.y <= 0:
+            self.y = self.v
+            self.d = new_d
+
+        if self.x >= width:
+            self.x = self.v
+            self.d = new_d
+
+        if self.y >= height:
+            self.y = self.v
+            self.d = new_d
+        
+
+        
+################################################################################
+# run code
+###############################################################################
 
 class FlockingModel(object):
     
@@ -197,84 +241,39 @@ class FlockingModel(object):
 
 
 
+
+
     # find neighbors and adjust boid speed, direction to mean
     def check_neighbors(self):
 
-        #----------------------------------------------------------------------------------
-        # count neighbors 2 steps away
-        for i in range(1, n_boids):         # skip zero for now will deal with 0 issues later
-
-            # get location of this bot
+        for i in range(n_boids):      
+            
+            # get location of this boid
             boid = self.boids[i]
             x = boid.x
             y = boid.y
 
-            # see if we have neighbors and stuff them in a list to process
-            # start at 0, end at n+1
-            #n_1 = self.board[x-1:x+2,y-1:y+2]
-            neighbors = self.board[x-min_distance:x+min_distance+1, y-min_distance:y+min_distance+1]
-            
-            # update x, y, velocity, direction and board
-            # yes, this includes self, that way we keep going same direction, velocity if no one else is nearby
-            neighbor_boids = np.unique(neighbors)
-            neighbor_boids = neighbor_boids[1:-1]       # remove zeros, lots of empty space around us
-            n_neighbors = len(neighbor_boids)
-            
-            #--------------------------------------------------------------------------------
+                        
+            # get neighbors boid can see
+            neighbors = neighborhood(x, y, self.boids)
+            n_neighbors = len(neighbors)
+
             # adapt speed and heading to local mean
-            if n_neighbors > 0:
-
-                velocity_mean = 0.
-                direction_mean = 0.
-                distance = 0.
-
-                for i in range(0, n_neighbors):     # skip 0
-                    #distance = np.sqrt((x - self.boids[int(neighbor_boids[i])].x) **2 +  (y - self.boids[int(neighbor_boids[i])].y) **2 )
-                    distance += 1       # divide by zero
-
-                    velocity_mean += self.boids[int(neighbor_boids[i])].velocity / distance **2
-                    direction_mean += self.boids[int(neighbor_boids[i])].direction / distance **2
-                    
-                velocity_mean /= n_neighbors
-                direction_mean /= n_neighbors
+            boid.update(neighbors)
+               
             
-            # no neighbors - maintain speed and heading
-            else:
-                velocity_mean = boid.velocity
-                direction_mean = boid.direction
-
-
-
-            # ------------------------------------------------------------------------
-            # move boid, update boid, update self.board
-
             # remove boid from self.board
-            self.board[boid.x][boid.y] = 0
-
-
-            # update boid x, y, velocity, direction
-            if velocity_mean < max_velocity: 
-                boid.velocity = velocity_mean
-
-            if direction_mean <= 7 and direction_mean >=0:
-                boid.direction = direction_mean
-
-            # move if not off edge of world or on top of another boid
-            boid.x, boid.y, boid.velocity, boid.direction = new_location(boid.x, boid.y, boid.velocity, boid.direction)
-
+            self.board[x][y] = 0
+            
             # update self. board
             self.board[boid.x][boid.y] = i
 
 
         
-
-
-
-
+    # next time step
     def step(self):
 
         self.next += 1
-
         self.check_neighbors()
 
         # make a copy of the board for animation 
@@ -293,16 +292,23 @@ class FlockingModel(object):
 
 
 
-########################################
+#####################################################################################
 # setup model
+#####################################################################################
+
 flockingModel = FlockingModel(n_steps)
 
+
+
+#####################################################################################
+# animation
+#####################################################################################
 
 # convert game board to an image
 def image_from_array(array):
 
     z_im = np.zeros((height, width, 3), 'uint8')
-
+    
     for i in range(n_boids):
         x = flockingModel.boids[i].x
         y = flockingModel.boids[i].y
